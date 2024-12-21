77861f02fbb1c3d0f458745cfc638f0bcde44738
Mario D'errico <derrico85@gmail.com>
2024-12-21 21:05:53 +0000

    navigation component

---
+++ src/components/navigationMenu/navigation.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { useNavigationLinks } from "../../hook/useNavigation";
+import { Li, Nav, Ul } from "../styleNavigation";
+
+function Navigation() {
+  const { links, isLinkActive } = useNavigationLinks();
+
+  return (
+    <Nav>
+      <Ul>
+        {links.map((link) => (
+          <Li
+            key={link.link}
+            style={{
+              fontWeight: isLinkActive(link) ? "bold" : "normal",
+              color: isLinkActive(link) ? "blue" : "black",
+            }}
+          >
+            <Link to={link.link}>{link.label}</Link>
+          </Li>
+        ))}
+      </Ul>
+    </Nav>
+  );
+}
+
+export default Navigation;
