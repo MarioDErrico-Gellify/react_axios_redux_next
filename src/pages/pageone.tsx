a2cc9192be7c5866fa36ab834d7fc46f3316b86b
Mario D'errico <derrico85@gmail.com>
2024-12-21 23:01:46 +0000

    added cooment, clear code, improove quality

--- src/pages/pageone.tsx
+++ src/pages/pageone.tsx
@@ -1,6 +1,12 @@
 import Navigation from "../components/navigation";
 //this is a referral page, this show only navigation menu
 function pageOne() {
-  return <Navigation></Navigation>;
+  return (
+    <>
+      {/*this component show only landing page, consits in navigation component*/}
+      <Navigation></Navigation>
+      <h1>Welcome to react showcase app, choose options in nav bar link</h1>
+    </>
+  );
 }
 export default pageOne;
