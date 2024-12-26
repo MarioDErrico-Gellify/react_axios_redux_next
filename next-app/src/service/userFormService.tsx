f8b61f5410b6aa29fa445a4852e7f04c4420445e
mario d'errico <marioderrico85@gmail.com>
2024-12-26 14:00:25 +0000

    chore: add .env with non-sensitive variables (JSONPlaceholder)
    Includes public API URL for <<development/testing>>. No sensitive data exposed.

--- next-app/src/service/userFormService.tsx
+++ next-app/src/service/userFormService.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 import { MantineFormDTO, retrieveFakeDataDto } from "./userFormTypes";
 
-const API_URL = process.env.REACT_APP_API_URL!;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function registerUser(params: MantineFormDTO) {
   try {
-    const response = await axios.post(API_URL, params);
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await axios.post(API_URL!, params, config);
     console.log("Dati ricevuti dalla chiamata API:", response.data);
     return response.data;
   } catch (error) {
