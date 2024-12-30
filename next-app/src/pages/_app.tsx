52b169706e715db5ee44938cb107e9a48eea6ced
MarioDErrico-Gellify <derrico85@gmail.com>
2024-12-30 23:03:13 +0100

    improved code quality

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import NavBar from "@/components/navBar/navBar";
-import {MantineProvider} from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 
-import {AppProps} from "next/app";
-import {Provider} from "react-redux";
-import {store} from "../store/store";
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <MantineProvider>
